3a44476bacef3a7225ebd4b1b23d67d088aea28c
Ion Ursachi <nunutu29@users.noreply.github.com>
2017-11-16 20:19:22 +0100

    Update ePriceScript.js

--- ePriceScript.js
+++ ePriceScript.js
@@ -1,20 +1,28 @@
 //Controlla ogni due secondi se è inziato l'evento oppure no
 
+var Emails;
+
 $(document).ready(
     function(){
         emailjs.init("user_gYkiwCJv1rvW8EUwu7e7A");
-        
+        Emails = 2;
         Check();
     }    
 );
 
+
+
 function Check(){
     if($("#bf_boxActive").hasClass("sel")){
         console.log("Evento In Corso");
+        if(Emails >= 0)
+        {
+            Emails--;
+            emailjs.send("gmail","eprice_johnny", {});
+        }
     }
     else if($("#bf_boxEnd").hasClass("sel")){
-        console.log("Evento Finito");
-        emailjs.send("gmail","eprice_johnny", {});
+        console.log("Evento Finito");        
     }
-    setTimeout(Check, 10000);
+    setTimeout(Check, 2000);
 }
